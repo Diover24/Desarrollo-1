659eb4d937e65f47239ace2e58b5bac665b70d2a
Diover24 <diover.sanchez@correo.tdea.edu.co>
2026-02-20 17:20:05 -0500

    funcion Creada

--- Actividad#1.js
+++ Actividad#1.js
@@ -1,8 +1,16 @@
 //numero random y adivinarlo
  Map.ramdom()
- const contenedor ={
-    adivinar: 0,
+ const User ={
+    adivinar: Math.floor(Math.random() * 10),
     intentos : 0,
-    estados: 0
+    estado: " "
  }
- 
+
+function Evaluar(Numero) {
+    User.intentos++; 
+    if (Numero===User.adivinar) {
+        User.estado = "Ganado"
+        return
+    }
+    return "Fallaste sigue intentando"
+}
